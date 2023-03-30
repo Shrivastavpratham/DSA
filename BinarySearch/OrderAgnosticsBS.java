45577746077dedf7a7381858e265fc98ac4eadd2
Pratham <prathamshrivastav5@gmail.com>
2023-03-30 20:20:34 +0530

    hello

--- BinarySearch/OrderAgnosticsBS.java
+++ BinarySearch/OrderAgnosticsBS.java
@@ -7,7 +7,7 @@ public class OrderAgnosticsBS {
         int ans = oabs(arr , target);
         System.out.println(ans);
     }
-    static int oabs(int [] arr, int target){
+    static int oabs(int [] arr, int target)
         int start = 0;
         int end = arr.length -1 ;
         //find the order of the array asscending or decending
@@ -16,7 +16,7 @@ public class OrderAgnosticsBS {
         while (start <= end) {
             // find the middle element
             //int mid = ( start + end ) / 2; // might be pssosnle that (start + int ) exceeds the range of int in java
-            int mid = start + (end- start) / 2;
+            int mid = start + (end - start) / 2;
 
             if(arr[mid]== target){
                 return mid;
